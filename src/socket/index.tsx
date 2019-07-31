8e5fee9e0a76eb56929da5c458b3f89f60809cce
Michael Yang <yang.michaelk@gmail.com>
2019-07-31 00:58:56 -0700

    Point to right site

--- src/socket/index.tsx
+++ src/socket/index.tsx
@@ -3,7 +3,7 @@ import { setGameData, navigateTo } from "../actions";
 import { setSocketId } from "../actions";
 
 const port = process.env.PORT 
-const socket = io("https://mingmilkgames.herokuapp.com:");
+const socket = io("https://mmmgame.herokuapp.com:");
 export class SocketListener {
   constructor(store) {
     socket.on("JOINED_GAME", game => {
