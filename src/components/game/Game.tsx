9913169ace32156fa3bc45e28e808f80af57fda6
Michael Yang <yang.michaelk@gmail.com>
2019-07-31 00:10:48 -0700

    all rounds

--- src/components/game/Game.tsx
+++ src/components/game/Game.tsx
@@ -262,7 +262,7 @@ class Game extends React.Component<GameStateProps, any> {
       <div className="Game">
         <RoleButton />
         <div style={{minHeight:"85px"}}>{this.showAnnouncment()}</div>
-        {/* <AllRounds rounds={rounds} failedVotes={failedVotes} currentRound={currentRound} /> */}
+        <AllRounds rounds={rounds} failedVotes={failedVotes} currentRound={currentRound} />
         <div style={{minHeight:"226px"}}>
         {/* <PlayerList/> */}
         {/* <VoteButtons roundStatus={roundStatus} players = {players}/> */}
