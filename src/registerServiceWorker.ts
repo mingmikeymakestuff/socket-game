da5f1a69ff19f6cef0feb0e69c5bace78329c27f
Michael Yang <yang.michaelk@gmail.com>
2019-07-31 02:38:27 -0700

    try service worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -34,7 +34,7 @@ export default function register() {
     }
 
     window.addEventListener("load", () => {
-      const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
+      const swUrl = "https://mmmgame.herokuapp.com/service-worker.js";
 
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
