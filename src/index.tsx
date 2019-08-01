c42bea45d2e0573d7ce0315be14c452391e2487c
Michael Yang <yang.michaelk@gmail.com>
2019-07-31 22:58:35 -0700

    Comment service worker for now

--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,4 @@ import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
 require('../src/App.css');
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+// registerServiceWorker();
